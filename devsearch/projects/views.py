380d5eaaecd571c00d890bc84f48daa1f6058c86
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-07-18 16:13:18 -0700

    Linked database models to templates through views.py, can now view sample data in browser

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,40 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 
 # Create your views here.
-projectsList = [
-    {
-        "id": "1",
-        "title": "Ecommerce Website",
-        "description": "Fully functional ecommerce website",
-    },
-    {
-        "id": "2",
-        "title": "Portfolio Website",
-        "description": "A personal website to write articles and display work",
-    },
-    {
-        "id": "3",
-        "title": "Social Network",
-        "description": "An open source project built by the community",
-    },
-]
 
 
 def projects(request):
-    page = "projects"
-    number = 10
+    projects = Project.objects.all()
+    context = {"projects": projects}
     return render(
         request,
         "projects/projects.html",
-        {"page": page, "number": number, "projects": projectsList},
+        context,
     )
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i["id"] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, "projects/single-project.html", {"project": projectObj})
