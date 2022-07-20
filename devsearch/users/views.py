49572a0081cb1d5c69e4c725f5a93fbca64c8ecb
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-07-19 23:51:01 -0700

    Created basic login page to showcase login route and view, add logout and flash messages for errors

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from click import password_option
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Profile
 
+
 # Create your views here.
+
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect("profiles")
+
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print("Username does not exist")
+            messages.error(request, "Username does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("profiles")
+        else:
+            print("Username OR password is incorrect")
+            messages.error(request, "Username OR password is incorrect")
+
+    return render(request, "users/login_register.html")
+
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, "User was logged out")
+    return redirect("login")
 
 
 def profiles(request):
