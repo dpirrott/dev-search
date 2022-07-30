be8af8f873ae19787d72e8bec81fcecab151bdfe
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-07-29 20:16:53 -0700

    Implement basic pagnation for profiles and projects pages, now need pagination to work with existing search queries

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Project, Tag
 from .forms import ProjectForm
-from .utils import searchProjects
+from .utils import searchProjects, paginateProjects
 
 
 # Create your views here.
@@ -11,21 +10,13 @@ from .utils import searchProjects
 
 def projects(request):
     projects, search_query = searchProjects(request)
+    custom_range, projects = paginateProjects(request, projects, 6)
 
-    page = request.GET.get("page")
-    results = 3
-    paginator = Paginator(projects, results)
-
-    try:
-        projects = paginator.page(page)
-    except PageNotAnInteger:
-        page = 1
-        projects = paginator.page(page)
-    except EmptyPage:
-        page = paginator.num_pages
-        projects = paginator.page(page)
-
-    context = {"projects": projects, "search_query": search_query}
+    context = {
+        "projects": projects,
+        "search_query": search_query,
+        "custom_range": custom_range,
+    }
     return render(
         request,
         "projects/projects.html",
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib.auth.models import User
 from django.db.models import Q
-from .utils import searchProfiles
+from .utils import paginateProfiles, searchProfiles, paginateProfiles
 from .models import Profile, Skill
 
 
@@ -73,7 +73,14 @@ def registerUser(request):
 
 def profiles(request):
     profiles, search_query = searchProfiles(request)
-    context = {"profiles": profiles, "search_query": search_query}
+
+    custom_range, profiles = paginateProfiles(request, profiles, 1)
+
+    context = {
+        "profiles": profiles,
+        "search_query": search_query,
+        "custom_range": custom_range,
+    }
     return render(request, "users/profiles.html", context)
 
 
