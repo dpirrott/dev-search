c4994caf5030baa16924f7bbf9a3314ce462b33a
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-07-31 00:35:23 -0700

    Implemented user messaging (non-websocket) with a send/receive message inbox system

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -44,3 +44,34 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    recipient = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="messages",
+    )
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False, null=True)
+
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return str(
+            self.name + " to " + self.recipient.name + " at " + str(self.created)
+        )
+
+    class Meta:
+        ordering = ["is_read", "-created"]
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from django.contrib.auth.models import User
 from .utils import paginateProfiles, searchProfiles, paginateProfiles
-from .models import Profile
+from .models import Profile, Message
 
 
 # Create your views here.
@@ -158,3 +158,52 @@ def deleteSkill(request, pk):
 
     context = {"object": skill}
     return render(request, "delete_template.html", context)
+
+
+@login_required(login_url="login")
+def inbox(request):
+    profile = request.user.profile
+    messageRequests = profile.messages.all()
+    unreadCount = messageRequests.filter(is_read=False).count()
+    context = {"messageRequests": messageRequests, "unreadCount": unreadCount}
+    return render(request, "users/inbox.html", context)
+
+
+@login_required(login_url="login")
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if not message.is_read:
+        message.is_read = True
+        message.save()
+    context = {"message": message}
+    return render(request, "users/message.html", context)
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+
+            message.save()
+
+            messages.success(request, "Your message was sent successfully sent!")
+            return redirect("user-profile", pk=recipient.id)
+
+    context = {"recipient": recipient, "form": form}
+    return render(request, "users/message_form.html", context)
